ce6f0e2e81ad251b06ba1e58fab280e788667d4b
ERII00 <121377921+ERII00@users.noreply.github.com>
2023-01-03 22:00:34 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,54 @@
+// $().ready(function(){
+// 	$('.tidao').click(function(){
+// 		c(hide);
+// 	})
+// })
+// 
+
+function hide(){
+			 $('#hid').fadeOut(5000);	 
+		       }
+			   
+function showimg(){
+		     $('#test').fadeIn(3000);	
+			 $('#test2').fadeIn(4000);
+			 $('#test3').fadeIn(5000);
+			 document.getElementById("myBar").style.display="block";
+		   }
+						  
+function move() {
+		  var elem = document.getElementById("myBar");   
+		  var width = 10;
+		  var id = setInterval(frame, 40);
+		  function frame() {
+		    if (width >= 100) {
+		      clearInterval(id);
+		    } else {
+		      width++; 
+		      elem.style.width = width + '%'; 
+		      elem.innerHTML = width * 1  + '%';
+		    }
+		  }
+		}
+		
+ function finsh(){
+	 setTimeout(function(){
+		 document.getElementById("wenzi").style.display="block";
+	 },3700);
+			 
+			 
+			 }		   
+
+								  
+// function c(showimg){
+// 	
+// 	showimg();
+// 	move();
+// 	finsh();
+// }
+
+
+
+
+
+
